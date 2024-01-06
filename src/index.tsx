21aeca2ec031805055785ab39429b06c8edbb5da
yan.jia <crazystone13753251592@gmail.com>
2024-01-06 20:37:29 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MineSweeper from "./mine-sweeper";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/sweeper",
+        element: <MineSweeper />,
+    },
+]);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/mine-sweeper/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MineSweeper() {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MineSweeper;
